e3672cbda764733e87c38684e0377d3cd71fdaee
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-10 03:15:42 +0600

    explore destructuring in typescript

---
+++ src/destructuring.ts
@@ -0,0 +1,27 @@
+//* object destructuring
+
+const user = {
+    id: 123,
+    name: {
+        firstName: "Saifullah",
+        middleName: "Kabir",
+        lastName: "Rana",
+    },
+    gender: 'Male',
+    favoriteColor: "Black",
+};
+
+//  const myFavoriteColor = user.favoriteColor;
+//  const myLastName = user.name.lastName;
+
+const { favoriteColor: myFavoriteColor, name: { lastName } } = user;
+// console.log(lastName);
+
+//* array destructuring
+
+const friends = ['Ablu', 'Bablu', 'Tablu'];
+
+// const myBestFriend = friends[1];
+
+const [, myBestFriend] = friends
+console.log(myBestFriend);
