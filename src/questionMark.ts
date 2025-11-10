a63e2b4c7960e5385b0d7c53ca46e43b8a125b4c
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-10 17:28:10 +0600

    explore ternary , nullish coalescing operator & optional chaining in ts

---
+++ src/questionMark.ts
@@ -0,0 +1,51 @@
+// ? : ternary operator : decision making
+// ?? : nullish coalescing operator : null/undefined
+// ?. : optional chaining
+
+//* ternary operator 
+const eligibleForMarried = (age: number) => {
+    // if(age >= 21) {
+    //     console.log('You are eligible');
+    // }else{
+    //     console.log('You are not eligible!');
+    // }
+
+    const result = age >= 21 ? 'You are eligible' : 'You are not eligible!';
+    console.log(result);
+}
+
+eligibleForMarried(22);
+
+//* nullish coalescing operator
+
+const userTheme = undefined;
+
+const selectedTheme = userTheme ?? 'Light Theme';
+console.log(selectedTheme);
+
+
+const isAuthenticated = '';
+
+const resultWithTernary = isAuthenticated ? isAuthenticated : "You are guest!";
+const resultWithNullish = isAuthenticated ?? "You are Guest!";
+
+console.log({ resultWithTernary }, { resultWithNullish });
+
+
+//* optional chaining
+
+const user: {
+    address: {
+        city: string
+        upazila: string,
+        postalCode? : string
+    }
+} = {
+    address: {
+        city: "Chattogram",
+        upazila: "Chandanish",
+    },
+};
+
+const postalCode = user?.address?.postalCode;
+console.log(postalCode);
