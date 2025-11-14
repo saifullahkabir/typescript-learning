c6057799e7f08485a84a02e77b1b50abe2401d20
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-14 20:37:42 +0600

    explore inheritance

---
+++ src/oop/inheritance.ts
@@ -0,0 +1,44 @@
+
+// common 
+class Person {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, addres: string) {
+        this.name = name;
+        this.age = age;
+        this.address = addres;
+    };
+
+    getSleep(numOfHours: number) {
+        console.log(`${this.name} ${numOfHours} gonta gumay`);
+    }
+}
+
+// student
+class Student extends Person { };
+
+const student1 = new Student('Mr. Fakibaz', 20, 'Bangladesh');
+
+student1.getSleep(10);
+
+// teacher
+class Teacher extends Person {
+    designation: string;
+
+    constructor(name: string, age: number, address: string, designation: string) {
+
+        super(name, age, address);
+
+        this.designation = designation;  // own property
+    };
+
+    // own method
+    getClass(numOfClass: number) {
+        console.log(`${this.name} ${numOfClass} gonta class neei`);
+    }
+}
+
+const teacher1 = new Teacher('Mr. Teacher', 35, 'Bangladesh', 'Senior Teacher');
+teacher1.getClass(4);
