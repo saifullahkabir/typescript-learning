9de8cd24c1b7b05040f76d8b07ef34ad84fc81c6
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-15 19:14:46 +0600

    explore access modifiers

---
+++ src/oop/accessModifiers.ts
@@ -0,0 +1,29 @@
+// access modifier
+
+class Bankaccount {
+    public readonly userId: number;
+    public userName: string;
+    protected userBalance: number;
+
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    };
+
+    addBalance(balance: number) {
+        this.userBalance = this.userBalance + balance;
+    }
+}
+
+class StudentBankAccount extends Bankaccount{
+    test(){
+        this.userBalance
+    }
+}
+
+const ranaAccount = new Bankaccount(111, 'Rana', 10);
+ranaAccount.addBalance(100);
+ranaAccount.addBalance(100);
+console.log(ranaAccount);
+
