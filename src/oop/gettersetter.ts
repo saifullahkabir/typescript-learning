167b7731a74ead899564e7f6c1b6272f554d99e7
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-15 21:11:51 +0600

    explore static in typescript

--- src/oop/gettersetter.ts
+++ src/oop/gettersetter.ts
@@ -1,41 +1,38 @@
 //* getter and setter
 
-
 class Bankaccount {
-    public readonly userId: number;
-    public userName: string;
-    protected userBalance: number;
+  public readonly userId: number;
+  public userName: string;
+  protected userBalance: number;
 
-    constructor(userId: number, userName: string, userBalance: number) {
-        this.userId = userId;
-        this.userName = userName;
-        this.userBalance = userBalance;
-    };
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
 
-    // set balance
-    // addBalance(balance: number) {
-    //     return this.userBalance = this.userBalance + balance;
-    // };
+  // set balance
+  // addBalance(balance: number) {
+  //     return this.userBalance = this.userBalance + balance;
+  // };
 
-    //* using setter for add balance
-    set addBalance(amount: number) {
-        this.userBalance = this.userBalance + amount;
-    }
+  //* using setter for add balance
+  set addBalance(amount: number) {
+    this.userBalance = this.userBalance + amount;
+  }
 
-    // get balance
-    // getBalance() {
-    //     return this.userBalance;
-    // }
+  // get balance
+  // getBalance() {
+  //     return this.userBalance;
+  // }
 
-    //* using getter for get balance
-    get getBalance() {
-        return this.userBalance
-    }
-
+  //* using getter for get balance
+  get getBalance() {
+    return this.userBalance;
+  }
 }
 
-
-const ranaAccount = new Bankaccount(111, 'Rana', 10);
+const ranaAccount = new Bankaccount(111, "Rana", 10);
 // ranaAccount.addBalance(100); // have to call function
 // ranaAccount.addBalance(200);
 
@@ -44,4 +41,3 @@ const ranaAccount = new Bankaccount(111, 'Rana', 10);
 ranaAccount.addBalance = 100; // don't have to call bcz using setter
 ranaAccount.addBalance = 200;
 console.log(ranaAccount.getBalance); // don't have to call bcz using getter
-
