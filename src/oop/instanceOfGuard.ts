4707c06a39ada2624d16a7f513d6e39a5846ec46
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-15 18:10:50 +0600

    explore instance of guard

---
+++ src/oop/instanceOfGuard.ts
@@ -0,0 +1,58 @@
+// oop: instance of type guard / type narrowing
+
+class Person {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    };
+
+    getSleep(numOfhours: number) {
+        console.log(`${this.name} daily ${numOfhours} ghonta  ghumay.`);
+    };
+};
+
+class Student extends Person {
+    constructor(name: string) {
+        super(name);
+    }
+
+    doStudy(numOfhours: number) {
+        console.log(`${this.name} daily ${numOfhours} ghonta study kore.`);
+    };
+}
+
+class Teacher extends Person {
+    constructor(name: string) {
+        super(name);
+    };
+
+    doClass(numOfhours: number) {
+        console.log(`${this.name} daily ${numOfhours} ghonta class neei.`);
+    }
+};
+
+// function guard
+const isStudent = (user: Person) => {
+    return user instanceof Student;  // user is Student
+};
+
+const isTeacher = (user: Person) => {
+    return user instanceof Teacher;  // user is Teacher
+}
+
+const getUserInfo = (user: Person) => {
+    if (isStudent(user)) {
+        user.doStudy(10);
+    } else if (isTeacher(user)) {
+        user.doClass(6);
+    } else{
+        user.getSleep(12);
+    }
+};
+
+// class er instance
+const student1 = new Student("Mr. Student");
+const teacher1 = new Teacher("Mr. Teaher");
+const person1 = new Person("Mr. Person");
+getUserInfo(student1);
